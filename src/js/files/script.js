82d986e20196df4af96bf73e6301a296a1f121fa
Ailas Monsy <ognevoy.bima@gmail.com>
2022-12-21 18:47:09 +0300

    all pages

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,9 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+const widthOrientir = document.querySelector(".width-orientir");
+const projectDesctiptionBlock = document.querySelector(".project-dcrn__main");
+if (widthOrientir) {
+  projectDesctiptionBlock.style.width = widthOrientir.offsetWidth + "px";
+}

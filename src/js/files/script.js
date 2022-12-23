c4993d93893c52ced4b92bda380dbc180bec222b
Ailas Monsy <ognevoy.bima@gmail.com>
2022-12-23 15:41:28 +0300

    sertificates

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -8,3 +8,18 @@ const projectDesctiptionBlock = document.querySelector(".project-dcrn__main");
 if (widthOrientir) {
   projectDesctiptionBlock.style.width = widthOrientir.offsetWidth + "px";
 }
+
+const imageItems = document.querySelectorAll(".project-il__item");
+const previewItem = document.querySelector(".project-dcrn__preview");
+
+imageItems.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    imageItems.forEach((eleme) => {
+      eleme.classList.remove("active");
+    });
+    element.classList.add("active");
+    previewItem.innerHTML = element.querySelector(
+      ".project-il__item_main"
+    ).innerHTML;
+  });
+});
